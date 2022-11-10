08813fd8b73cebd0ceed3315ad80492b6349e28c
holahapi <holahapi@gmail.com>
2022-11-10 17:47:08 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         diy_test
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @updateURL    https://github.com/holahapi/diy_tampermonkey/blob/main/test.txt
+// @downloadURL  https://github.com/holahapi/diy_tampermonkey/blob/main/test.txt
+// @description  try to take over the world!
+// @author       You
+// @match        https://lihkg.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=lihkg.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+alert("this is v 0.2");
+    // Your code here...
+})();
